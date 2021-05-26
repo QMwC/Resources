9c42bba237933e2387935a90b425978974deb4fb
QMwC <70219418+QMwC@users.noreply.github.com>
2021-05-25 22:49:55 -0400

    Added postMessage so iframe can communicate height.

--- courses/math/other.js
+++ courses/math/other.js
@@ -2,3 +2,8 @@ let back = document.createElement("button");
 back.onclick = () => window.history.back();
 back.innerText = "<-- Go back to course structure";
 document.body.insertBefore(back, document.body.firstChild);
+window.onmessage = (e) => {
+  if (e.origin === "https://quantum.georgetown.domains") {
+    window.parent.postMessage(document.body.parentElement.offsetHeight, "https://quantum.georgetown.domains");
+  }
+}

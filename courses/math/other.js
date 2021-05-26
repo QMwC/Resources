c5f8dbbde9ddbd6c3787841072acf09d8dcd840c
QMwC <70219418+QMwC@users.noreply.github.com>
2021-05-25 23:53:21 -0400

    Alternate between offsetHeight and scrollHeight.

--- courses/math/other.js
+++ courses/math/other.js
@@ -2,8 +2,15 @@ let back = document.createElement("button");
 back.onclick = () => window.history.back();
 back.innerText = "<-- Go back to course structure";
 document.body.insertBefore(back, document.body.firstChild);
+let msg = 0;
 window.onmessage = (e) => {
   if (e.origin === "https://quantum.georgetown.domains") {
-    window.parent.postMessage(document.body.parentElement.scrollHeight, "https://quantum.georgetown.domains");
+    if (msg) {
+      window.parent.postMessage(document.body.parentElement.offsetHeight, "https://quantum.georgetown.domains");
+      msg = 0;
+    } else {
+      window.parent.postMessage(document.body.parentElement.scrollHeight, "https://quantum.georgetown.domains");
+      msg = 1;
+    }
   }
 }

ae3fa3fbc34c3cd3cafd1bfe504c75d1a586740b
QMwC <70219418+QMwC@users.noreply.github.com>
2021-05-25 23:16:56 -0400

    scrollHeight instead of offsetHeight

--- courses/math/other.js
+++ courses/math/other.js
@@ -4,6 +4,6 @@ back.innerText = "<-- Go back to course structure";
 document.body.insertBefore(back, document.body.firstChild);
 window.onmessage = (e) => {
   if (e.origin === "https://quantum.georgetown.domains") {
-    window.parent.postMessage(document.body.parentElement.offsetHeight, "https://quantum.georgetown.domains");
+    window.parent.postMessage(document.body.parentElement.scrollHeight, "https://quantum.georgetown.domains");
   }
 }
